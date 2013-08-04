707a086be2cb5d9eb0e401b3cf563b49f615d9ac
Chris Seymour <chris.j.seymour@hotmail.com>
2013-08-04 12:40:50 +0100

    Turn off cursor, Docstrings, Linted

--- gol.py
+++ gol.py
@@ -26,17 +26,18 @@ class gol(object):
         curses.start_color()
         curses.noecho()
         curses.cbreak()
+        curses.curs_set(0)
         self.grid = {}
-        self.x, self.y = (0,0)
+        self.x, self.y = (0, 0)
         self.height, self.width = self.screen.getmaxyx()
-        self.padding = 3
+        self.y_pad = 3
+        self.x_pad = 2
         self.char = "*"
         self.generations = args.g
         self.rate = args.r
         self.win = curses.newwin(self.height, self.width, self.y, self.x)
         self.win.box()
-        self.win.addstr(1 ,2, "Game of Life")
-
+        self.win.addstr(1, 2, "Game of Life")
 
     def __del__(self):
         self.win.clear()
@@ -44,14 +45,15 @@ class gol(object):
         curses.echo()
         curses.endwin()
 
-
     def DrawGrid(self):
-
+        """
+        Redraw the grid with the new generation
+        """
         cGrid = copy.deepcopy(self.grid)
 
-        for i in range(self.padding, self.height-self.padding):
-            for j in range(self.padding, self.width-self.padding):
-                cell = (i,j)
+        for i in range(self.y_pad, self.height - self.y_pad):
+            for j in range(self.x_pad, self.width - self.x_pad):
+                cell = (i, j)
                 n = self.CountNeighbours(cell)
                 if cell in self.grid.keys():
                     self.win.addch(i, j, self.char)
@@ -67,65 +69,75 @@ class gol(object):
         self.win.refresh()
         return
 
-
-    def CountNeighbours(self,cell):
+    def CountNeighbours(self, cell):
+        """
+        Return the number active neighbours
+        within one positions away from cell
+        """
         count = 0
         x, y = cell
-        for i in range(x-1, x+2):
-            for j in range(y-1, y+2):
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
                 if (i, j) in self.grid.keys() and (i, j) != cell:
                     count += 1
         return count
 
-
     def RandomStart(self, n):
+        """
+        Initialise the game with n random points
+        """
         for _ in range(0, n):
-            ry = random.randint(self.padding, self.height-self.padding)
-            rx = random.randint(self.padding, self.width-self.padding)
-            self.grid[(ry,rx)] = 1
+            ry = random.randint(self.y_pad, self.height - self.y_pad)
+            rx = random.randint(self.x_pad, self.width - self.x_pad)
+            self.grid[(ry, rx)] = 1
 
-        self.win.addstr(self.height-3, 2, "Grid: %i   " % len(self.grid))
-        self.win.addstr(self.height-2, 2, "Generation: 0")
+        self.win.addstr(self.height - 3, 2, "Grid: %i   " % len(self.grid))
+        self.win.addstr(self.height - 2, 2, "Generation: 0")
         self.DrawGrid()
         sleep(self.rate)
         return
 
     def TestStart(self):
+        """
+        Initialise the game with a predefined set
+        up where the behaviour is deterministic
+        """
 
-        #Blinker
-        self.grid[(4,4)] = 1
-        self.grid[(4,5)] = 1
-        self.grid[(4,6)] = 1
+        # Blinker
+        self.grid[(4, 4)] = 1
+        self.grid[(4, 5)] = 1
+        self.grid[(4, 6)] = 1
 
-        #Toad
-        self.grid[(9,5)] = 1
-        self.grid[(9,6)] = 1
-        self.grid[(9,7)] = 1
-        self.grid[(10,4)] = 1
-        self.grid[(10,5)] = 1
-        self.grid[(10,6)] = 1
+        # Toad
+        self.grid[(9, 5)] = 1
+        self.grid[(9, 6)] = 1
+        self.grid[(9, 7)] = 1
+        self.grid[(10, 4)] = 1
+        self.grid[(10, 5)] = 1
+        self.grid[(10, 6)] = 1
 
-        #Glider
-        self.grid[(4,11)] = 1
-        self.grid[(5,12)] = 1
-        self.grid[(6,10)] = 1
-        self.grid[(6,11)] = 1
-        self.grid[(6,12)] = 1
+        # Glider
+        self.grid[(4, 11)] = 1
+        self.grid[(5, 12)] = 1
+        self.grid[(6, 10)] = 1
+        self.grid[(6, 11)] = 1
+        self.grid[(6, 12)] = 1
 
-        self.win.addstr(self.height-3, 2, "Grid: %i   " % len(self.grid))
-        self.win.addstr(self.height-2, 2, "Generation: 0")
+        self.win.addstr(self.height - self.y_pad,
+                        self.x_pad, "Grid: %i   " % len(self.grid))
+        self.win.addstr(self.height - self.x_pad, self.x_pad, "Generation: 0")
         self.DrawGrid()
         sleep(self.rate)
         return
 
-
     def Breed(self):
-        for i in range(1,self.generations+1):
-            self.win.addstr(self.height-3, 2, "Grid: %i   " % len(self.grid))
-            self.win.addstr(self.height-2, 2, "Generation: %s" % i)
+        for i in range(1, self.generations + 1):
+            self.win.addstr(self.height - 3, 2, "Grid: %i   " % len(self.grid))
+            self.win.addstr(self.height - 2, 2, "Generation: %s" % i)
             self.DrawGrid()
             sleep(self.rate)
         return
+
 
 def main(args):
     game = gol(args)
@@ -136,9 +148,9 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", default=50, type=int,
-                        help = "The number of generations")
+                        help="The number of generations")
     parser.add_argument("-r", default=0.02, type=float,
-                        help = "The refresh rate")
+                        help="The refresh rate")
     parser.add_argument("-n", default=300, type=int,
-                        help = "The number of initial points")
+                        help="The number of initial points")
     main(parser.parse_args())
